1c5654e331b4d1159b773c25c086dee62e042222
Jeremy Miller <jmiller3346@gmail.com>
2017-09-21 21:12:13 -0500

    bumping version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures hms'
 long_description 'Installs/Configures hms'
 issues_url 'https://github.com/som3guy/hms/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/som3guy/hms' if respond_to?(:source_url)
-version '0.1.0'
+version '0.2.0'
 
 depends 'docker'
 depends 'yum-epel'
